0cf968e799a903d60173741f5b752e3912fb8396
fahadmapari <btechy4@gmail.com>
2025-07-04 02:34:50 +0530

    feat: Enhance SearchBlock with dynamic country and city selection
    - Integrated useSWR for fetching countries and cities from a JSON file.
    - Updated CountrySelector and CitySelector components to accept props for dynamic rendering.
    - Added state management for selected country and city using useState.
    - Modified DatePicker to set the initial date to today.

--- src/components/common/DatePicker.tsx
+++ src/components/common/DatePicker.tsx
@@ -37,6 +37,7 @@ export default function DatePicker() {
           selected={date}
           onSelect={setDate}
           initialFocus
+          fromDate={new Date()}
         />
       </PopoverContent>
     </Popover>
--- src/components/dashboard/SearchBlock.tsx
+++ src/components/dashboard/SearchBlock.tsx
@@ -12,17 +12,56 @@ import {
 import DatePicker from "../common/DatePicker";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import useSWR from "swr";
+import axios from "axios";
+
+interface CountryCityType {
+  name: string;
+  cities: string[];
+}
 
 const SearchBlock = () => {
   const { push } = useRouter();
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+
+  const { data: countriesAndCities, isLoading } = useSWR(
+    ["countries", "cities"],
+    async () => {
+      const response = await axios.get<CountryCityType[]>(
+        "/json/countries_cities.json"
+      );
+
+      // TODO: optimize the json file later
+      const countries = response.data.map((country) => country.name);
+      const cities: Record<string, string[]> = {};
+      response.data.forEach((country) => {
+        cities[country.name] = country.cities;
+      });
+
+      return { countries, cities };
+    }
+  );
+
   return (
     <div className="w-[800px] p-4 border border-gray-300 rounded-lg  bg-white">
       <div className="text-center text-2xl font-semibold pb-4">
         Welcome, John Doe.
       </div>
       <div className="flex items-center gap-4">
-        <CountrySelector />
-        <CitySelector />
+        <CountrySelector
+          countries={countriesAndCities?.countries}
+          selectedCountry={selectedCountry}
+          setSelectedCountry={(country: string) => setSelectedCountry(country)}
+        />
+        <CitySelector
+          selectedCity={selectedCity}
+          setSelectedCity={(city: string) => setSelectedCity(city)}
+          selectedCountry={selectedCountry}
+          cities={countriesAndCities?.cities}
+        />
         <DatePicker />
         <Button
           className="flex-1 cursor-pointer"
@@ -35,40 +74,66 @@ const SearchBlock = () => {
   );
 };
 
-const CountrySelector = () => {
+const CountrySelector = ({
+  countries,
+  selectedCountry,
+  setSelectedCountry,
+}: {
+  countries?: string[];
+  selectedCountry: string;
+  setSelectedCountry: (country: string) => void;
+}) => {
   return (
-    <Select>
+    <Select value={selectedCountry} onValueChange={setSelectedCountry}>
       <SelectTrigger className="w-full">
         <SelectValue placeholder="Country" />
       </SelectTrigger>
-      <SelectContent>
+      <SelectContent className="max-h-56">
         <SelectGroup>
           <SelectLabel>Countries</SelectLabel>
-          <SelectItem value="india">India</SelectItem>
-          <SelectItem value="uae">UAE</SelectItem>
-          <SelectItem value="usa">United States</SelectItem>
-          <SelectItem value="mexico">Mexico</SelectItem>
-          <SelectItem value="brazil">Brazil</SelectItem>
+          {countries ? (
+            countries.map((country) => (
+              <SelectItem value={country} key={country}>
+                {country}
+              </SelectItem>
+            ))
+          ) : (
+            <span>No countries found</span>
+          )}
         </SelectGroup>
       </SelectContent>
     </Select>
   );
 };
 
-const CitySelector = () => {
+const CitySelector = ({
+  selectedCountry,
+  selectedCity,
+  setSelectedCity,
+  cities,
+}: {
+  selectedCountry: string;
+  selectedCity: string;
+  setSelectedCity: (city: string) => void;
+  cities?: Record<string, string[]>;
+}) => {
   return (
-    <Select>
+    <Select value={selectedCity} onValueChange={setSelectedCity}>
       <SelectTrigger className="w-full">
         <SelectValue placeholder="City" />
       </SelectTrigger>
-      <SelectContent>
+      <SelectContent className="max-h-56">
         <SelectGroup>
           <SelectLabel>Cities</SelectLabel>
-          <SelectItem value="india">Mumbai</SelectItem>
-          <SelectItem value="uae">Abu Dhabi</SelectItem>
-          <SelectItem value="usa">Newyork</SelectItem>
-          <SelectItem value="mexico">Delhi</SelectItem>
-          <SelectItem value="brazil">Sao Paulo</SelectItem>
+          {cities ? (
+            cities[selectedCountry]?.map((city) => (
+              <SelectItem value={city} key={city}>
+                {city}
+              </SelectItem>
+            ))
+          ) : (
+            <span>No cities found</span>
+          )}
         </SelectGroup>
       </SelectContent>
     </Select>
