7d95ca6a0b982e3505eb355ecd05fef4240b7b16
fahadmapari <btechy4@gmail.com>
2025-07-04 17:24:31 +0530

    navbar updatw

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex items-center justify-between px-6 py-4 border-b border-white/20">
+    <div className="flex items-center justify-between px-6 py-4 border-b border-gray-200">
       <div>
         <h3>LocalHost</h3>
       </div>
